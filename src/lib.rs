d9b903b2f23ce24c53944696cf83c6a95cc52b57
ItsMeSKKYZ3R <71552277+ItsMeSKKYZ3R@users.noreply.github.com>
2022-08-02 13:56:30 +0200

    First version

---
+++ src/lib.rs
@@ -0,0 +1,73 @@
+use std::any::Any;
+
+/// A simple struct that contains checking functions
+pub struct Checker;
+
+/// Checker struct implementation
+impl Checker {
+    /// Verify the type of variable with a generic as a type.
+    ///
+    /// This function takes a reference to any of your type and check if the type into the generic is the same that the type of your data.
+    /// ```
+    /// use checker::Checker;
+    ///
+    /// fn main() {
+    ///     let result: bool = Checker::type_of::<i32>(&"test".to_string());
+    ///
+    ///     match result {
+    ///         true => println!("Correct type"),
+    ///         false => println!("Incorrect type"),
+    ///     }
+    ///
+    ///     // Expected result: Correct type
+    /// }
+    /// ```
+    pub fn type_of<T: Any>(var: &dyn Any) -> bool {
+        var.is::<T>()
+    }
+
+    /// Executes a logical equal on both datas
+    ///
+    /// All datas needs to derive PartialEq.
+    /// ```
+    /// use checker::Checker;
+    ///
+    /// #[derive(PartialEq)]
+    /// struct Test {
+    ///     a: u8,
+    /// }
+    ///
+    /// fn main() {
+    ///     let a: Test = Test {
+    ///         a: 2,
+    ///     };
+    ///     let b: Test = Test {
+    ///         a: 8,
+    ///     };
+    ///
+    ///     let result: bool = Checker::equals_to(a, b);
+    ///
+    ///     match result {
+    ///         true => println!("Equal!"),
+    ///         false => println!("Not equal"),
+    ///     }
+    ///
+    ///     // Expected result: Equal!
+    /// }
+    /// ```
+    pub fn equals_to<T: PartialEq>(a: T, b: T) -> bool {
+        a == b
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_type() {
+        let result = Checker::type_of::<String>(&"test".to_string());
+
+        assert_eq!(result, true)
+    }
+}
